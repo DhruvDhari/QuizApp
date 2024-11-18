3798faa107cf6b4f882e8c3487b6425a00c961b5
Dhruv Dhari <108563803+DhruvDhari@users.noreply.github.com>
2024-11-18 08:02:32 +0530

    Update FullScreenPrompt.js

--- src/components/FullScreenPrompt.js
+++ src/components/FullScreenPrompt.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FullScreenPrompt = ({ onRequestFullScreen }) => {
   return (
     <div className="fullscreen-prompt">
-    <a href="https://docs.google.com/document/d/1dNAuV7T5rP7StRLo4r-oMQ4HpAeSY_CB/edit?usp=drivesdk&ouid=106792302852462244170&rtpof=true&sd=true">Visit W3Schools.com!</a>
+    <a href="https://docs.google.com/document/d/1dNAuV7T5rP7StRLo4r-oMQ4HpAeSY_CB/edit?usp=drivesdk&ouid=106792302852462244170&rtpof=true&sd=true">Get RLDL Practical File</a>
       <h2>Please enable full-screen mode to start the quiz.</h2>
       <button onClick={onRequestFullScreen}>Enable Full Screen</button>
     </div>

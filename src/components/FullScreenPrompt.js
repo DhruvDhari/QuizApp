4254e3261b9ce863fc43c7d3526d9c4b5e63e4cd
Dhruv Dhari <108563803+DhruvDhari@users.noreply.github.com>
2024-11-25 07:55:14 +0530

    Update FullScreenPrompt.js

--- src/components/FullScreenPrompt.js
+++ src/components/FullScreenPrompt.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FullScreenPrompt = ({ onRequestFullScreen }) => {
   return (
     <div className="fullscreen-prompt">
-    <a href="https://docs.google.com/document/d/1eAtdvW2MqZV-6ObCrjXjEaIUcITpvI95/edit?usp=drivesdk&ouid=106792302852462244170&rtpof=true&sd=true">Get dwdm Practical File</a>
+    <a href=" https://drive.google.com/file/d/1eyMFZaYfR7AtoP-r4q0fW0fBQ0a_Jden/view?usp=drivesdk ">Get ngw Practical File</a>
       <h2>Please enable full-screen mode to start the quiz.</h2>
       <button onClick={onRequestFullScreen}>Enable Full Screen</button>
     </div>
